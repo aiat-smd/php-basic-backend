14887180e0538219724429fd90ac15c839436fc9
Aiat <isamidinovaiat@gmail.com>
2020-11-09 00:30:32 +0600

    form handler

--- backend/index.php
+++ backend/index.php
@@ -3,6 +3,19 @@
 $uri = $_SERVER["REQUEST_URI"];
 $method = $_SERVER["REQUEST_METHOD"];
 
+
 if ($uri == "/"){
-    include "./home.php";
+    include "./pages/home.php";
+}
+elseif ($uri =="/posts") {
+    include "./pages/posts.php";
+}
+elseif ($uri =="/login") {
+    include "./pages/form.php";
+}
+elseif (preg_match("/fromForm.*/", $uri)) {
+    include "./pages/form_handler.php";
+}
+else{
+    include "./pages/error_404.php";
 }
---
+++ backend/pages/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    
+    <form action="/fromForm">
+        <label for="login">Login:</label>
+        <input id="login" type="text" name="login">
+        <br>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" type="text" name="password">
+        <br>
+        <br>
+        <button>Submit</button>
+    </form>
+
+</body>
+</html>
